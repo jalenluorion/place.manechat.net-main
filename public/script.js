67de173fe39946ea8c6830d4207ab15e7d22e9eb
Jalen Lu <jalenlu2000@icloud.com>
2024-02-03 18:50:01 -0700

    Revert "one mo"
    This reverts commit afde3c6d40d8a8bf719db8fa74c9eb909d935a45.

--- public/script.js
+++ public/script.js
@@ -124,7 +124,7 @@ async function connectSocket()
 {
 	await repaintCanvas();
 
-	socket = new WebSocket("ws://" + window.location.host);
+	socket = new WebSocket("wss://" + window.location.host);
 
 	socket.addEventListener("message", async e =>
 	{
